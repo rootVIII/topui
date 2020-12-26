58f291d9bde00cc85537c87054d0d318a133963f
solsticenet <solsticenet@mm1.myfiosgateway.com>
2020-12-26 02:31:58 -0500

    set size of listBox

--- main.go
+++ main.go
@@ -32,8 +32,8 @@ func (t *TopUI) RunApp() {
 	ui := widgets.NewQApplication(len(os.Args), os.Args)
 
 	t.window = widgets.NewQMainWindow(nil, 0)
-	t.window.SetMinimumSize2(400, 675)
-	t.window.SetMaximumSize2(400, 675)
+	t.window.SetMinimumSize2(450, 675)
+	t.window.SetMaximumSize2(450, 675)
 	t.window.SetWindowTitle("Top UI")
 
 	h1 := widgets.NewQHBoxLayout()
@@ -47,11 +47,12 @@ func (t *TopUI) RunApp() {
 	timer1.Start(1000)
 
 	title := widgets.NewQGraphicsScene(t.window)
-	title.AddText("t e m p", gui.NewQFont2("Menlo", 20, 1, false))
+	title.AddText("t e m p", gui.NewQFont2("Menlo", 15, 1, false))
 	titleView := widgets.NewQGraphicsView(t.window)
 	titleView.SetScene(title)
 
 	t.listBox = widgets.NewQListWidget(t.window)
+	t.listBox.SetFixedHeight(525)
 
 	t.intervalLabel = widgets.NewQLabel(t.window, 0)
 	t.intervalLabel.SetText("g r e e t i n g s")
