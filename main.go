2296298880b58c24e774ad75c1a19aa9671d56b2
solsticenet <solsticenet@mm1.myfiosgateway.com>
2020-12-26 02:05:21 -0500

    initial

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"os"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
+	"github.com/therecipe/qt/widgets"
+)
+
+// TopUI represents the main application window.
+type TopUI struct {
+	window *widgets.QMainWindow
+	// inputTextBox              *widgets.QLineEdit
+	intervalLabel *widgets.QLabel
+}
+
+// reset the gui and variables to inital/empty values.
+func (t *TopUI) reset() {
+
+	// t.inputTextBox.SetText(t.PDFIn)
+	t.window.SetWindowTitle("")
+}
+
+func (t *TopUI) updateUI() {
+
+}
+
+// RunApp runs the GUI version of the app. Check for new jobs and process
+// found jobs in a single background goroutine to prevent the hanging GUI
+// and possible spinning beach-ball for larger-sized PDFs.
+func (t *TopUI) RunApp() {
+
+	ui := widgets.NewQApplication(len(os.Args), os.Args)
+
+	t.window = widgets.NewQMainWindow(nil, 0)
+	t.window.SetMinimumSize2(400, 675)
+	t.window.SetMaximumSize2(400, 675)
+	t.window.SetWindowTitle("Top UI")
+
+	h1 := widgets.NewQHBoxLayout()
+	h2 := widgets.NewQHBoxLayout()
+	h3 := widgets.NewQHBoxLayout()
+
+	v := widgets.NewQVBoxLayout()
+
+	timer1 := core.NewQTimer(t.window)
+	timer1.ConnectTimeout(func() { t.updateUI() })
+	timer1.Start(1000)
+
+	title := widgets.NewQGraphicsScene(nil)
+	title.AddText("t e m p", gui.NewQFont2("Menlo", 20, 1, false))
+	titleView := widgets.NewQGraphicsView(nil)
+	titleView.SetScene(title)
+
+	tempLabel := widgets.NewQLabel(nil, 0)
+	tempLabel.SetText("placeholder for horizontal 2")
+
+	t.intervalLabel = widgets.NewQLabel(nil, 0)
+	t.intervalLabel.SetText("g r e e t i n g s")
+
+	intervalButton := widgets.NewQPushButton2("Browse", nil)
+	intervalButton.ConnectClicked(func(bool) { t.updateUI() })
+
+	h1.Layout().AddWidget(titleView)
+	h2.Layout().AddWidget(tempLabel)
+	h3.Layout().AddWidget(intervalButton)
+	h3.Layout().AddWidget(t.intervalLabel)
+
+	for _, layout := range []*widgets.QHBoxLayout{h1, h2, h3} {
+		v.AddLayout(layout, 0)
+	}
+
+	widget := widgets.NewQWidget(nil, 0)
+	widget.SetLayout(v)
+	t.window.SetCentralWidget(widget)
+	t.window.Show()
+	ui.Exec()
+}
+
+func main() {
+
+	var ui = &TopUI{}
+	ui.RunApp()
+}
