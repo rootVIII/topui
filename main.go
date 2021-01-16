b2cc505de967292253ded78aff55242cdee5a06e
solsticenet <solsticenet@mm1.myfiosgateway.com>
2021-01-16 03:12:46 -0500

    organize methods for building GUI

--- main.go
+++ main.go
@@ -14,40 +14,25 @@ import (
 	"github.com/therecipe/qt/widgets"
 )
 
-// 2020 rootVIII - List top processes in a simple QT GUI
+// 2021 rootVIII - List top processes in a simple QT GUI
 
-// TopUI represents the main application window.
+// TopUI represents the main application type
+// and controls/updates the GUI in real time.
 type TopUI struct {
-	window    *widgets.QMainWindow
-	tableBox  *widgets.QTableWidget
-	cmdBuffer [][]string
+	window         *widgets.QMainWindow
+	tableBox       *widgets.QTableWidget
+	titleView      *widgets.QGraphicsView
+	verticalLayout *widgets.QVBoxLayout
+	cmdBuffer      [][]string
+	rowMax         int
 }
-
-// func (t *TopUI) scanSTDOUT(scanner *bufio.Scanner) {
-// 	var index int
-// 	for scanner.Scan() {
-// 		line := strings.Fields(scanner.Text())
-// 		if len(line) > 0 {
-// 			if _, err := strconv.Atoi(line[0]); err == nil {
-// 				if line[1] != "top" && line[1] != "topui" {
-// 					t.tableBox.SetItem(index, 0, widgets.NewQTableWidgetItem2(line[0], 0))
-// 					t.tableBox.SetItem(index, 1, widgets.NewQTableWidgetItem2(line[2], 0))
-// 					t.tableBox.SetItem(index, 2, widgets.NewQTableWidgetItem2(line[1], 0))
-// 					index++
-// 				}
-// 			} else {
-// 				index = 0
-// 			}
-// 		}
-// 	}
-// }
 
 func (t *TopUI) scanSTDOUT(scanner *bufio.Scanner) {
 	for scanner.Scan() {
 		line := strings.Fields(scanner.Text())
 		if len(line) > 0 {
 			if _, err := strconv.Atoi(line[0]); err == nil {
-				if line[1] != "top" && line[1] != "topui" {
+				if line[1] != "top" && line[1] != "topui" && line[1] != "qtbox" {
 					t.cmdBuffer = append(t.cmdBuffer, []string{line[0], line[2], line[1]})
 				}
 			}
@@ -58,9 +43,12 @@ func (t *TopUI) scanSTDOUT(scanner *bufio.Scanner) {
 func (t *TopUI) unloadBuffer() {
 	if len(t.cmdBuffer) > 0 {
 		for index, line := range t.cmdBuffer {
-			t.tableBox.SetItem(index, 0, widgets.NewQTableWidgetItem2(line[0], 0))
-			t.tableBox.SetItem(index, 1, widgets.NewQTableWidgetItem2(line[1], 0))
-			t.tableBox.SetItem(index, 2, widgets.NewQTableWidgetItem2(line[2], 0))
+			if index > t.rowMax {
+				break
+			}
+			for col := 0; col < 3; col++ {
+				t.tableBox.SetItem(index, col, widgets.NewQTableWidgetItem2(line[col], 0))
+			}
 		}
 		t.cmdBuffer = nil
 	}
@@ -79,57 +67,76 @@ func (t TopUI) execTop() (*bufio.Scanner, error) {
 	return bufio.NewScanner(stdout), nil
 }
 
+func (t *TopUI) setWindow() {
+	t.window = widgets.NewQMainWindow(nil, 0)
+	t.window.SetMinimumSize2(480, 675)
+	t.window.SetMaximumSize2(480, 675)
+	t.window.SetWindowTitle("💻 ")
+}
+
+func (t *TopUI) setTitle() {
+	title := widgets.NewQGraphicsScene(t.window)
+	title.AddText("Top Processes", gui.NewQFont2("Menlo", 12, 1, false))
+	t.titleView = widgets.NewQGraphicsView(t.window)
+	t.titleView.SetScene(title)
+	t.titleView.SetFixedHeight(30)
+}
+
+func (t *TopUI) setTable() {
+	t.tableBox = widgets.NewQTableWidget(t.window)
+	t.tableBox.SetFixedHeight(610)
+	t.tableBox.SetColumnCount(3)
+	t.rowMax = 500
+	t.tableBox.SetRowCount(t.rowMax + 1)
+	t.tableBox.SetHorizontalHeaderLabels([]string{"PID", "CPU%", "APP"})
+	t.tableBox.VerticalHeader().Hide()
+	t.tableBox.SetColumnWidth(0, 80)
+	t.tableBox.SetColumnWidth(1, 80)
+	t.tableBox.SetColumnWidth(2, 270)
+}
+
+func (t *TopUI) showWindow() {
+	widget := widgets.NewQWidget(nil, 0)
+	widget.SetLayout(t.verticalLayout)
+	t.window.SetCentralWidget(widget)
+	t.window.Show()
+}
+
+func (t *TopUI) setVLayout() {
+	h1 := widgets.NewQHBoxLayout()
+	h2 := widgets.NewQHBoxLayout()
+	t.verticalLayout = widgets.NewQVBoxLayout()
+	h1.Layout().AddWidget(t.titleView)
+	h2.Layout().AddWidget(t.tableBox)
+
+	for _, layout := range []*widgets.QHBoxLayout{h1, h2} {
+		t.verticalLayout.AddLayout(layout, 0)
+	}
+}
+
+func (t *TopUI) buildApp() {
+	t.setWindow()
+	t.setTitle()
+	t.setTable()
+	t.setVLayout()
+	t.showWindow()
+
+	timer := core.NewQTimer(t.window)
+	timer.ConnectTimeout(func() { t.unloadBuffer() })
+	timer.Start(1000)
+}
+
 // RunApp initializes the GUI and all associated GUI types.
 func (t *TopUI) RunApp() {
+	ui := widgets.NewQApplication(len(os.Args), os.Args)
+	t.buildApp()
+
 	scanner, err := t.execTop()
 	if err != nil {
 		panic(err)
 	}
 
 	go t.scanSTDOUT(scanner)
-
-	ui := widgets.NewQApplication(len(os.Args), os.Args)
-
-	t.window = widgets.NewQMainWindow(nil, 0)
-	t.window.SetMinimumSize2(480, 675)
-	t.window.SetMaximumSize2(480, 675)
-	t.window.SetWindowTitle("💻 ")
-
-	h1 := widgets.NewQHBoxLayout()
-	h2 := widgets.NewQHBoxLayout()
-	v := widgets.NewQVBoxLayout()
-
-	title := widgets.NewQGraphicsScene(t.window)
-	title.AddText("Top Processes", gui.NewQFont2("Menlo", 12, 1, false))
-	titleView := widgets.NewQGraphicsView(t.window)
-	titleView.SetScene(title)
-	titleView.SetFixedHeight(30)
-
-	t.tableBox = widgets.NewQTableWidget(t.window)
-	t.tableBox.SetFixedHeight(610)
-	t.tableBox.SetColumnCount(3)
-	t.tableBox.SetRowCount(500)
-	t.tableBox.SetHorizontalHeaderLabels([]string{"PID", "CPU%", "APP"})
-	t.tableBox.VerticalHeader().Hide()
-	t.tableBox.SetColumnWidth(0, 80)
-	t.tableBox.SetColumnWidth(1, 80)
-	t.tableBox.SetColumnWidth(2, 270)
-
-	h1.Layout().AddWidget(titleView)
-	h2.Layout().AddWidget(t.tableBox)
-
-	for _, layout := range []*widgets.QHBoxLayout{h1, h2} {
-		v.AddLayout(layout, 0)
-	}
-
-	widget := widgets.NewQWidget(nil, 0)
-	widget.SetLayout(v)
-	t.window.SetCentralWidget(widget)
-	t.window.Show()
-
-	timer := core.NewQTimer(t.window)
-	timer.ConnectTimeout(func() { t.unloadBuffer() })
-	timer.Start(1000)
 
 	ui.Exec()
 }
